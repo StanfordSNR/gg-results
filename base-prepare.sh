3f4e6c6a34c6267249aed8834d167c4f09d5add8
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-18 16:02:33 -0700

    Make all the scripts executable.

--- base-prepare.sh
+++
@@ -1,5 +0,0 @@
-source info
-source 1-prepare.sh
-
-TIMECOMMAND_PREP=/usr/bin/time --output=${TEST_PREP_OUTPUT?"not set"} --verbose
-TIMECOMMAND=/usr/bin/time --output=${TEST_OUTPUT?"not set"} --verbose
