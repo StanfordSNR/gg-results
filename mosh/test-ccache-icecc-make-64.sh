a9008fc053e218761e3f9f53addb22a839d63ea9
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-18 15:47:48 -0700

    Add define.sh to ccache scripts.

--- mosh/test-ccache-icecc-make-64.sh
+++ mosh/test-ccache-icecc-make-64.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+source define.sh
+
 export PATH=/usr/lib/ccache:$PATH
 export CCACHE_PREFIX=icecc
+
 ${TIMECOMMAND} make -j64
--- mosh/test-ccache-local-make-64.sh
+++ mosh/test-ccache-local-make-64.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 
+source define.sh
+
 export PATH=/usr/lib/ccache:$PATH
 ${TIMECOMMAND} make -j64
