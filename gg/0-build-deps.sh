6d371de8df96f29b0ae54aa26064ee85bef1cfa6
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-18 10:13:49 -0700

    install-common-deps.sh: Add -y option to add-apt-repo.

--- gg/0-build-deps.sh
+++ gg/0-build-deps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-sudo apt-get install libcap-dev libcajun-dev libkeyutils-dev \
-                     libncurses5-dev libboost-dev libssl-dev autopoint \
-                     help2man texinfo protobuf-compiler libprotobuf-dev \
-                     libcrypto++-dev
+sudo apt-get install -y libcap-dev libcajun-dev libkeyutils-dev \
+                        libncurses5-dev libboost-dev libssl-dev autopoint \
+                        help2man texinfo protobuf-compiler libprotobuf-dev \
+                        libcrypto++-dev
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 sudo pip3 install protobuf
--- install-common-deps.sh
+++ install-common-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-sudo add-apt-repository ppa:keithw/gcc-backport
+sudo add-apt-repository -y ppa:keithw/gcc-backport
 sudo apt-get update -qq
 
 sudo apt-get install -y gcc-7 g++-7 automake autoconf libtool make curl \
