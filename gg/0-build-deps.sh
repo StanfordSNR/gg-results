3e40dcdd29249914a8bdcb0427a868e4e8d47ff9
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-18 10:36:05 -0700

    gg/build-deps.sh: Add python2.7 to the list of dependencies.

--- gg/0-build-deps.sh
+++ gg/0-build-deps.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y libcap-dev libcajun-dev libkeyutils-dev \
                         libncurses5-dev libboost-dev libssl-dev autopoint \
                         help2man texinfo protobuf-compiler libprotobuf-dev \
-                        libcrypto++-dev
+                        libcrypto++-dev python
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 sudo pip3 install protobuf
