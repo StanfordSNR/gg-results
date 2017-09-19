bfd68e1b07b2813646661e5132c78995722b4f7c
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-18 21:20:32 -0700

    mosh: Purge gg-job-server cache before running tests.

--- mosh/test-gg-lambda-1000.sh
+++ mosh/test-gg-lambda-1000.sh
@@ -2,6 +2,9 @@
 
 source ${BASH_SOURCE%/*}/define.sh
 
+# Purge gg-job-server cache
+curl http://${GG_RUNNER_SERVER}/clearall -X POST
+
 ${TIMECOMMAND_PREP0} gg-run make -j${SMALL_CORES}
 
 export GG_LAMBDA=1
