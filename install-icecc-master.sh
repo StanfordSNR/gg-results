c134a282911862f65f230d29d1c307e49486f7fd
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-20 13:02:30 -0700

    Limit icecc local jobs to 1.

--- install-icecc-master.sh
+++ install-icecc-master.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
 sudo apt-get install -y icecc
+
+sudo sed -i 's/ICECC_MAX_JOBS=""/ICECC_MAX_JOBS="1"/' /etc/icecc/icecc.conf
+
 sudo service icecc-scheduler restart
 sudo service iceccd restart
