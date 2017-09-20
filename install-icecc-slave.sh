c3148bbd76fc06219a17d564b9a5aec78da6940a
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-20 12:34:50 -0700

    Restart slave icecc server.

--- install-icecc-slave.sh
+++ install-icecc-slave.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y icecc
 read -p "icecc master ip address: " ICECC_MASTER
 sudo sed -i "/ICECC_SCHEDULER_HOST=\"\"/c\ICECC_SCHEDULER_HOST=\"${ICECC_MASTER}\"" /etc/icecc/icecc.conf
 
-sudo service iceccd start
+sudo service iceccd restart
