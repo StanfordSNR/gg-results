171b7a0a6a5b38db4db7522d88abe56b4b7c4717
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-20 15:04:10 -0700

    Fix the preparation for llvm and cmake.

--- llvm/1-prepare.sh
+++ llvm/1-prepare.sh
@@ -6,10 +6,24 @@ rm -rf build/
 mkdir -p build
 
 pushd build/
-if [ -z ${__GG_TEST+x} ]; then
-  cmake ../source/;
-else
-  gg-init
-  CXX="g++" CC="gcc" GG_BYPASS=1 gg-run cmake ../source/;
-fi
+
+case "${__TEST_TYPE}" in
+  MAKE)
+    cmake ../source/
+    ;;
+
+  ICECC)
+    CXX="g++" CC="gcc" PATH=/usr/lib/icecc/bin:$PATH cmake ../source/
+    ;;
+
+  GG)
+    gg-init
+    CXX="g++" CC="gcc" GG_BYPASS=1 gg-run cmake ../source/
+    ;;
+
+  CCACHE)
+    CXX="g++" CC="gcc" PATH=/usr/lib/ccache:$PATH cmake ../source/
+    ;;
+esac
+
 popd
