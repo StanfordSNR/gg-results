564297f8a6e8553c86d8a09992741c714460243d
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-21 15:06:22 -0700

    llvm: Do gg tests remotely.

--- llvm/test-gg-lambda-1000.sh
+++ llvm/test-gg-lambda-1000.sh
@@ -5,11 +5,11 @@ source ${BASH_SOURCE%/*}/define.sh
 # Purge gg-job-server cache
 curl --fail http://${GG_RUNNER_SERVER}/cgi-bin/gg/reset.cgi
 
-gg-init
-${TIMECOMMAND_PREP0} gg-run make -j${SMALL_CORES} ${__MAKE_TARGETS}
-
 export GG_LAMBDA=1
 export GG_REMOTE=1
 export GG_MAXJOBS=${LAMBDA_CORES}
+
+gg-init
+${TIMECOMMAND_PREP0} gg-run make -j${SMALL_CORES} ${__MAKE_TARGETS}
 
 ${TIMECOMMAND} gg-reduce ${__GG_TARGETS}
