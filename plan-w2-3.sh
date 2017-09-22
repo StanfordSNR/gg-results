2a9acdb2dfe3f981d7a25acbe7eed7143bdb08ab
Ubuntu <ubuntu@ip-172-31-41-124.us-west-2.compute.internal>
2017-09-22 09:08:43 +0000

    plan-w2-3.sh: Minor update to the plan.

--- plan-w2-3.sh
+++ plan-w2-3.sh
@@ -2,8 +2,8 @@
 
 export GG_S3_BUCKET=gg-ec2-us-west-2
 
-./run-test ffmpeg-minrel icecc-make-64 5
 ./run-test ffmpeg-minrel gg-ec2-64 5
+./run-test ffmpeg-minrel icecc-make-64 5
 ./run-test clang-minrel icecc-make-64 5
 ./run-test clang-minrel gg-ec2-64 5
 
