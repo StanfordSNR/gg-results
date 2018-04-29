8c68880585bbefed609c80615f0585d50cf0eaed
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-29 15:01:12 -0700

    Add -y.

--- gg/0-install-deps.sh
+++ gg/0-install-deps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-sudo apt-get install gcc-7 g++-7 protobuf-compiler libprotobuf-dev \
-                     libcrypto++-dev libcap-dev libkeyutils-dev \
-                     libncurses5-dev libboost-dev libssl-dev autopoint help2man \
-                     libhiredis-dev texinfo automake libtool pkg-config
+sudo apt-get install -y gcc-7 g++-7 protobuf-compiler libprotobuf-dev \
+                        libcrypto++-dev libcap-dev libkeyutils-dev \
+                        libncurses5-dev libboost-dev libssl-dev autopoint help2man \
+                        libhiredis-dev texinfo automake libtool pkg-config
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 sudo pip3 install protobuf
