c12d46b06582178f0f3d6f278043a68141e7a0e4
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-29 15:59:54 -0700

    env/setup.sh: Set git username and email.

--- env/setup.sh
+++ env/setup.sh
@@ -4,3 +4,6 @@ cp awscreds.sh ~/.awscreds.sh
 cp ggrc ~/.ggrc
 cp /etc/bash_completion.d/git-prompt ~/.git-prompt.sh
 cat append-to-bashrc >>~/.bashrc
+
+git config --global user.name "Sadjad Fouladi"
+git config --global user.email "sfouladi@gmail.com"
