eb12c61c823269e9f87b3e494da9e9f194eec99e
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-29 15:02:18 -0700

    Add python-minimal to the list of gg's dependencies.

--- gg/0-install-deps.sh
+++ gg/0-install-deps.sh
@@ -3,7 +3,8 @@
 sudo apt-get install -y gcc-7 g++-7 protobuf-compiler libprotobuf-dev \
                         libcrypto++-dev libcap-dev libkeyutils-dev \
                         libncurses5-dev libboost-dev libssl-dev autopoint help2man \
-                        libhiredis-dev texinfo automake libtool pkg-config
+                        libhiredis-dev texinfo automake libtool pkg-config \
+                        python-minimal
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 sudo pip3 install protobuf
