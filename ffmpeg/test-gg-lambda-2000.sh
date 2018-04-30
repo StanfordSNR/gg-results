7e6183c89bc93e851d7e17b31308344fc09d86db
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-29 18:00:28 -0700

    ffmpeg: Add timeout option for Lambda run.

--- ffmpeg/test-gg-lambda-2000.sh
+++ ffmpeg/test-gg-lambda-2000.sh
@@ -2,7 +2,6 @@
 
 source ${BASH_SOURCE%/*}/define.sh
 
-
 gg-init
 ${TIMECOMMAND_PREP0} gg-infer make -j${SMALL_CORES}
-${TIMECOMMAND} gg-force --engine lambda --jobs ${LARGE_CORES} ${__TARGETS?"not set"}
+${TIMECOMMAND} gg-force --engine lambda --jobs ${LAMBDA_CORES} --timeout 2 ${__TARGETS?"not set"}
