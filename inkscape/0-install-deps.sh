0fa0a60e7f1b344b7a6a18b41b0a1f3451902f53
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-29 19:53:25 -0700

    Fix the deps for inkscape.

--- inkscape/0-install-deps.sh
+++ inkscape/0-install-deps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
 
-sudo apt-get build-dep -y inkscape libsoup2.4-dev libgtkmm-3.0-dev \
-                          libgdk3.0-cil-dev libgdl-3-dev
+sudo apt-get build-dep -y inkscape
+
+sudo apt-get install -y libsoup2.4-dev libgtkmm-3.0-dev \
+                        libgdk3.0-cil-dev libgdl-3-dev
