957d5cf1dc8a44da1575c5526017a61d32d020b0
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-30 14:51:10 -0700

    Minor update to objectrec script.

--- objectrec/test-gg-lambda-2000-c25.sh
+++ objectrec/test-gg-lambda-2000-c25.sh
@@ -6,4 +6,4 @@ gg-init
 ./gen_vid_thunks.py -l li25-static
 
 ${TIMECOMMAND_PREP0} ./gen_thunks.sh
-${TIMECOMMAND} gg-force --engine lambda --timeout 20 --status --jobs ${LAMBDA_CORES} ${__TARGETS?"not set"}
+${TIMECOMMAND} gg-force --engine lambda --jobs ${LAMBDA_CORES} ${__TARGETS?"not set"}
