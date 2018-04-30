df4a66c4a01ebe61a9cec0a5f6be30852996749e
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-29 17:04:10 -0700

    Fix icecc slave installation script.

--- install-icecc-slave.sh
+++ install-icecc-slave.sh
@@ -3,6 +3,6 @@
 sudo apt-get install -y icecc
 
 read -p "icecc master ip address: " ICECC_MASTER
-sudo sed -i "s/^ICECC_SCHEDULER_HOST=\"\"/ICECC_SCHEDULER_HOST="${ICECC_MASTER}\"/" /etc/icecc/icecc.conf
+sudo sed -i "s/^ICECC_SCHEDULER_HOST=\"\"/ICECC_SCHEDULER_HOST=\"${ICECC_MASTER}\"/" /etc/icecc/icecc.conf
 
 sudo service iceccd restart
