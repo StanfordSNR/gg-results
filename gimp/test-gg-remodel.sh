ddbdbb766b2dc5d8127e58020f46f760d3e2319e
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-17 14:02:39 -0700

    Add test-icecc-48 for targets (except Chrome).

--- gimp/test-gg-remodel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-source ${BASH_SOURCE%/*}/define.sh
-
-export GG_METAINFER=1
-
-gg-init
-${TIMECOMMAND_PREP0} gg-infer make -j${SMALL_CORES}
-${TIMECOMMAND} gg-remodel ${__TARGETS?"not set"}
