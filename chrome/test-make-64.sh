faf3b5f5de75eae2d070007672b0810fe50a4b0d
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-16 23:53:34 -0700

    Fix the chrome test script.

--- chrome/test-make-64.sh
+++ chrome/test-make-64.sh
@@ -2,4 +2,6 @@
 
 source ${BASH_SOURCE%/*}/define.sh
 
+pushd out/Release
 ${TIMECOMMAND} ninja -j${LARGE_CORES} chrome
+popd
--- chrome/test-make.sh
+++ chrome/test-make.sh
@@ -2,4 +2,6 @@
 
 source ${BASH_SOURCE%/*}/define.sh
 
+pushd out/Release
 ${TIMECOMMAND} ninja -j1 chrome
+popd
