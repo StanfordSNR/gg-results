a8c9f7b695cf9d8fce6fb28fb513f8a1d54a8670
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-18 12:39:03 -0700

    Add lambda test for inkscape.

--- config.sh
+++ config.sh
@@ -5,3 +5,7 @@ export LARGE_CORES=48
 export ICECC_CORES=$(($LARGE_CORES+1))
 export ICECC_LARGE_CORES=$((8*$LARGE_CORES+1))
 export LAMBDA_CORES=2000
+export LAMBDA_MEDIUM_CORES=1000
+export LAMBDA_SMALL_CORES=500
+
+export LAMBDA_ENGINES="--jobs ${LAMBDA_CORES} --engine lambda=us-west-2 --engine lambda=us-east-1 --jobs ${LAMBDA_MEDIUM_CORES} --engine lambda=us-west-1 --engine lambda=us-east-2"
--- inkscape/test-gg-lambda-2000.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-source ${BASH_SOURCE%/*}/define.sh
-
-gg-init
-${TIMECOMMAND_PREP0} gg-infer make -j${SMALL_CORES} ${__MAKE_TARGETS}
-${TIMECOMMAND} gg-force --engine lambda --jobs ${LAMBDA_CORES} --timeout 2 ${__GG_TARGETS?"not set"}
