d2a7a7c453b05156f48c65a04071aa72c615baeb
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-18 15:37:02 -0700

    Add gg-48x8 test scripts for ffmpeg and gimp.

---
+++ gimp/test-gg-48x8.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+
+source ${BASH_SOURCE%/*}/define.sh
+
+gg-init
+tar xf ../data/remotes.tar.gz -C ../data
+
+for i in $(seq 1 8)
+do
+  REMOTE=GG_REMOTE_$i
+  curl --fail http://${!REMOTE}:9924/reset
+done
+
+export GG_GCC_BUILD_DIR=$(readlink -f .)
+export GG_CACHE_DIR=$(readlink -f ../data/)
+
+cat >headers.gg.txt <<EOL
+.h
+.c
+.inc
+EOL
+
+${TIMECOMMAND_PREP0} gg-build-infer make -j${SMALL_CORES}
+${TIMECOMMAND} gg-force ${REMOTE_ENGINES} --timeout 4 ${__TARGETS?"not set"}
+
+rm -rf ../data/remotes/
