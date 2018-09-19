f6ebeda534698b7bd3ae475dadd0fc6151283620
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-18 23:41:59 -0700

    Increase timeout interval for Chrome.

--- chrome/test-gg-lambda.sh
+++ chrome/test-gg-lambda.sh
@@ -53,11 +53,11 @@ popd
 unset GG_GCC_BUILD_DIR
 
 ${TIMECOMMAND_PREP7} gg-build-infer ninja -j${SMALL_CORES} v8_context_snapshot_generator
-${TIMECOMMAND_PREP8} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 10 v8_context_snapshot_generator *.so
+${TIMECOMMAND_PREP8} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 30 v8_context_snapshot_generator *.so
 
 # (6) and for my next trick, i'm gonna build chrome
 ${TIMECOMMAND_PREP9} gg-build-infer ninja -j${SMALL_CORES} chrome
-${TIMECOMMAND} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 10 chrome
+${TIMECOMMAND} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 30 chrome
 
 popd
 
