14347702cc968cfd1f65ef4dae629e18052fac13
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-19 10:50:02 -0700

    chrome/test-gg-lambda.sh: Increase timeout multiplier to 4.

--- chrome/test-gg-lambda.sh
+++ chrome/test-gg-lambda.sh
@@ -67,14 +67,14 @@ sleep 10
 unset GG_GCC_BUILD_DIR
 
 ${TIMECOMMAND_PREP7} gg-build-infer ninja -j${SMALL_CORES} v8_context_snapshot_generator
-${TIMECOMMAND_PREP8} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 30 --timeout-multiplier 2 v8_context_snapshot_generator *.so
+${TIMECOMMAND_PREP8} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 30 --timeout-multiplier 3 v8_context_snapshot_generator *.so
 
 echo "stage 5: done."
 sleep 10
 
 # (6) and for my next trick, i'm gonna build chrome
 ${TIMECOMMAND_PREP9} gg-build-infer ninja -j${SMALL_CORES} chrome
-${TIMECOMMAND} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 30 --timeout-multiplier 2 chrome
+${TIMECOMMAND} gg-force ${LARGE_LAMBDA_ENGINES} ${FALLBACK_ENGINES} --timeout 30 --timeout-multiplier 3 chrome
 
 echo "stage 6: done."
 
