b77f6b7f6350802c3f6819929031043f83ba5157
Sadjad Fouladi <sfouladi@gmail.com>
2019-01-05 13:36:21 -0800

    Install python3-distutils.

--- gg/0-install-deps.sh
+++ gg/0-install-deps.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y gcc-7 g++-7 protobuf-compiler libprotobuf-dev \
                         libcrypto++-dev libcap-dev libkeyutils-dev \
                         libncurses5-dev libboost-dev libssl-dev autopoint help2man \
                         libhiredis-dev texinfo automake libtool pkg-config \
-                        python-minimal
+                        python-minimal python3-distutils
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 sudo pip3 install protobuf
